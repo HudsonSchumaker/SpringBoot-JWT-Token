401bfc74744be2a99ddef560119a5bb3049f6839
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-03-21 16:41:32 +0100

    Create CursoService.java

---
+++ src/main/java/br/com/schumaker/forum/service/CursoService.java
@@ -0,0 +1,19 @@
+package br.com.schumaker.forum.service;
+
+import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import br.com.schumaker.forum.repository.CursoRepository;
+
+@Service
+public class CursoService() {
+
+  public List<Curso> getBy(ReportDTO dto) {
+    List<Funcionario> cursos = cursoRepository.findAll(Specification
+				.where(
+						CursoSpecification.nome(dto.getNome))
+						.or(CursoSpecification.categoria(dto.getCategoria))
+				);
+    return cursos;
+  }
+}
