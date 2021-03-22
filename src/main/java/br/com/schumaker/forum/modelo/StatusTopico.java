e7982dbe4df7c2bbfbb30cf296b3c443a7f6f43d
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-03-22 13:35:34 +0100

    Update StatusTopico.java

--- src/main/java/br/com/schumaker/forum/modelo/StatusTopico.java
+++ src/main/java/br/com/schumaker/forum/modelo/StatusTopico.java
@@ -2,9 +2,30 @@ package br.com.schumaker.forum.modelo;
 
 public enum StatusTopico {
 
-    NAO_RESPONDIDO,
-    NAO_SOLUCIONADO,
-    SOLUCIONADO,
-    FECHADO;
-
+    NAO_RESPONDIDO {
+		@Override
+		public StatusTopico getNextStatus() {
+			return NAO_SOLUCIONADO;
+		}
+	},
+    NAO_SOLUCIONADO{
+		@Override
+		public getNextStatus() {
+			return SOLUCIONADO;
+		}
+	},
+    SOLUCIONADO {
+		@Override
+		public StatusTopico getNextStatus() {
+			return FECHADO;
+		}
+	},
+    FECHADO {
+		@Override
+		public StatusTopico getNextStatus() {
+			return FECHADO;
+		}
+	};
+    
+    public abstract getNextStatus();
 }
